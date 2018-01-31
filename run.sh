09b17b090c9d25d32cb25423a3790f7c53d6c22d
Kwangmyung <kmkim@whatap.io>
2018-01-31 17:34:06 +0900

    fix run error

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 CUR_PATH=$(pwd)
-docker run -d -p 8080:8080 -p 8009:8009 -v $CUR_PATH/webapps:/opt/tomcat/webapps --name tomcat8 tomcat:8
+docker run -d -p 8080:8080 -p 8009:8009 -v $CUR_PATH/webapps:/opt/tomcat/webapps --name tomcat8 xmlangel/tomcat
